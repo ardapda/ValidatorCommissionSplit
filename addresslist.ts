5d08f5662e853fcf3f0543c42a3f3bf9d207320b
silentnoname <635397523pl@gamil.com>
2022-04-21 13:28:54 +0800

    add example in addresslist.ts

--- addresslist.ts
+++ addresslist.ts
@@ -14,3 +14,13 @@ export const addresslist: { [key: string]: any } = {
         ]
     },
     }
+
+    /* example
+    "mooncat-1-1":{
+        validator_address:"crevaloper1ph8l4v5gne3l0n7sj2208rthqdfjexhy97w2y5",
+        delegator_address:[
+            "cre1eflngqx2sj82yxuw4l7u5j2rf96vn9wnp55w6k",
+            "cre17vaf4sadxszkjt0zdwm5xa3q85y3uz4a3pxy7h",
+        ]
+    },
+     */
